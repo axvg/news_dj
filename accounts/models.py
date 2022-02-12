55de59483a6caa26e14f71b4466400c96b56c847
axvg <a52vega@gmail.com>
2022-02-12 14:27:23 -0500

    add custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 
-
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
--- config/settings.py
+++ config/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = "accounts.CustomUser" 
